c96dd1dfffcdb4290c122357fa59a719cdd6b2b5
Chan Kit <chankit@chans-air.mynet>
2023-10-16 22:40:35 +0800

    semi working travel web

--- base.js
+++ base.js
@@ -18,27 +18,33 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const system_message = "You are a travel planning assistant and follow\
-these steps based on the user input of the destination, the travel dates.\
-Step 1: #### Calculate the number of days the user is going to stay in the destination.\
-Step 2: #### Find at least 2 activites that's commonly done together by traveller based on online reviews.\
-Step 3: #### Find at least 2 restaurants near the activites with high reviews.\
-Step 4: #### Generate a travel itinerary for the user in the follwoing format:\
-Day 1:\n Activity 1: (description)\
-Activity 2: (description)\
-Lunch: (description)\
-Dinner: (description)"
+const system_message = "You are a travel planning assistant and follow \
+these steps based on the user input of destination and dates.\
+Step 1: #### Calculate the number of days the user is going to stay in the country and display {} days.\
+Step 2: #### If the user did not specify city of the country, suggest few popular cities in the country (at least 2 days per city) without asking. \
+Step 3: #### Take into account the distance between cities you suggest are the optimal route.\
+Step 4: #### Based on the number of days, suggest activites with desciptions and specific restaurants with high online reviews.\
+Step 6: #### Generate itinerary based on the previous steps in this format:\
+Day {}:\
+Morning: {} with relevant image\
+Afternoon: {}\
+Evening: {}\
+Lunch: {}\
+Dinner: {} with relevant image";
 
 app.post('/getAnswer', async (req, res) => {
   try {
     const userInput = req.body.userInput;
+    const userInput2 = req.body.userInput2;
 
     const response = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
         {"role": "system", "content": system_message},
         {"role": "user", "content": `${userInput}`},
+        {"role": "user", "content": `${userInput2}`},
       ],
+      temperature: 0.5,
     });
 
     res.setHeader('Content-Type', 'application/json');
@@ -48,47 +54,6 @@ app.post('/getAnswer', async (req, res) => {
   }
 });
 
-
 app.listen(5500, () => {
  console.log('http://localhost:5500/getAnswer');
 });
-
-
-/*const userInterface = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    });
-
-userInterface.prompt(); 
-userInterface.on('line', async function (input) {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: input }],
-      model: 'gpt-3.5-turbo',
-    });
-    console.log(chatCompletion.choices[0].message.content);
-    userInterface.prompt();
-  }); */
-
-/*  async function main() {
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: 'user', content: "Hey hello" }],
-      model: 'gpt-3.5-turbo',
-    });
-    console.log(chatCompletion.choices[0].message.content);
-}
-
-main(); */
-/*app.post("/getAnswer", async(req, res) => {
-  const userInput = req.body;
-
-  const chatCompletion = await openai.chat.completions.create({
-  messages: [{role: "user", content: `${userInput}`}],
-  model:"gpt-3.5-turbo",
-  })
-      res.json({
-          chatCompletion: chatCompletion.choices[0].message,
-  })
-}); 
-req.body.userInput
-
-*/
--- prompt.py
+++ prompt.py
@@ -1,11 +1,7 @@
 # prompt engineering to manipulate the result of the prompt
-import os
 import openai
-import sys
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
 
-openai.api_key  = os.environ['']
+openai.api_key  = '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'
 
 def get_completion_from_messages(messages, 
                                  model="gpt-3.5-turbo", 
@@ -18,107 +14,27 @@ def get_completion_from_messages(messages,
     )
     return response.choices[0].message["content"]
 
-delimiter = "####"
-system_message = f"""
-Follow these steps to answer the customer queries.
-The customer query will be delimited with four hashtags,\
-i.e. {delimiter}. 
+system_message = f""" You are a travel planning assistant and follow \
+these steps based on the user input of destination and dates.\
+Step 1: #### Calculate the number of days the user is going to stay in the country and display () days.\
+Step 2: #### If the user did not specify city of the country, suggest few popular cities in the country (at least 2 days per city) without asking. \
+Step 3: #### Take into account the distance between cities you suggest are the optimal route.\
+Step 4: #### Based on the number of days, suggest activites with desciptions and specific restaurants with high online reviews.\
+Step 6: #### Generate itinerary based on the previous steps in this format:\
+Day ():\
+Morning: () with relevant image\
+Afternoon: ()\
+Evening: ()\
+Lunch: ()\
+Dinner: () with relevant image"""
 
-Step 1:{delimiter} First decide whether the user is \
-asking a question about a specific product or products. \
-Product cateogry doesn't count. 
+user_message = f""" France, 15 Oct 2023 - 18 Oct 2023."""
 
-Step 2:{delimiter} If the user is asking about \
-specific products, identify whether \
-the products are in the following list.
-All available products: 
-1. Product: TechPro Ultrabook
-   Category: Computers and Laptops
-   Brand: TechPro
-   Model Number: TP-UB100
-   Warranty: 1 year
-   Rating: 4.5
-   Features: 13.3-inch display, 8GB RAM, 256GB SSD, Intel Core i5 processor
-   Description: A sleek and lightweight ultrabook for everyday use.
-   Price: $799.99
-
-2. Product: BlueWave Gaming Laptop
-   Category: Computers and Laptops
-   Brand: BlueWave
-   Model Number: BW-GL200
-   Warranty: 2 years
-   Rating: 4.7
-   Features: 15.6-inch display, 16GB RAM, 512GB SSD, NVIDIA GeForce RTX 3060
-   Description: A high-performance gaming laptop for an immersive experience.
-   Price: $1199.99
-
-3. Product: PowerLite Convertible
-   Category: Computers and Laptops
-   Brand: PowerLite
-   Model Number: PL-CV300
-   Warranty: 1 year
-   Rating: 4.3
-   Features: 14-inch touchscreen, 8GB RAM, 256GB SSD, 360-degree hinge
-   Description: A versatile convertible laptop with a responsive touchscreen.
-   Price: $699.99
-
-4. Product: TechPro Desktop
-   Category: Computers and Laptops
-   Brand: TechPro
-   Model Number: TP-DT500
-   Warranty: 1 year
-   Rating: 4.4
-   Features: Intel Core i7 processor, 16GB RAM, 1TB HDD, NVIDIA GeForce GTX 1660
-   Description: A powerful desktop computer for work and play.
-   Price: $999.99
-
-5. Product: BlueWave Chromebook
-   Category: Computers and Laptops
-   Brand: BlueWave
-   Model Number: BW-CB100
-   Warranty: 1 year
-   Rating: 4.1
-   Features: 11.6-inch display, 4GB RAM, 32GB eMMC, Chrome OS
-   Description: A compact and affordable Chromebook for everyday tasks.
-   Price: $249.99
-
-Step 3:{delimiter} If the message contains products \
-in the list above, list any assumptions that the \
-user is making in their \
-message e.g. that Laptop X is bigger than \
-Laptop Y, or that Laptop Z has a 2 year warranty.
-
-Step 4:{delimiter}: If the user made any assumptions, \
-figure out whether the assumption is true based on your \
-product information. 
-
-Step 5:{delimiter}: First, politely correct the \
-customer's incorrect assumptions if applicable. \
-Only mention or reference products in the list of \
-5 available products, as these are the only 5 \
-products that the store sells. \
-Answer the customer in a friendly tone.
-
-Use the following format:
-Step 1:{delimiter} <step 1 reasoning>
-Step 2:{delimiter} <step 2 reasoning>
-Step 3:{delimiter} <step 3 reasoning>
-Step 4:{delimiter} <step 4 reasoning>
-Response to user:{delimiter} <response to customer>
-
-Make sure to include {delimiter} to separate every step.
-"""
-
-
-user_message = f"""
-by how much is the BlueWave Chromebook more expensive \
-than the TechPro Desktop"""
-
-messages =  [  
+messages =  [ 
 {'role':'system', 
  'content': system_message},    
 {'role':'user', 
- 'content': f"{delimiter}{user_message}{delimiter}"},  
+ 'content': user_message},  
 ] 
 
 response = get_completion_from_messages(messages)
